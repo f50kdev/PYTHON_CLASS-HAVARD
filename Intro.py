51a0c8a96963f9d4cd6352426ea41647cf314576
f50kdev <f50kdev@example.com>
2025-10-23 15:22:34 +0530

    modify print fucntion

--- Intro.py
+++ Intro.py
@@ -5,3 +5,12 @@ name = input(" What's your name ?")
 message  = " welcome mister"
 #show slide
 print( message , name)
+#another way to print  using concatenation
+#real value of print function
+#  print() = print(*object ,sep="" , end = '/n' ,  file =sys.stdout , flush = flase)
+
+
+print("WELCOME MISTER " ,sep=""  + name , end="\n")
+print("02" , "12" , 2001)
+
+
