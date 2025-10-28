87cdb3cc76f9232f847db5d454c78aec0e6a1263
f50kdev <f50kdev@example.com>
2025-10-29 04:53:55 +0530

    function

--- Parametres.py
+++ Parametres.py
@@ -8,4 +8,3 @@ print(f'Welcome mister %s'  %name ).strip().title()
 #capitalizetion 
 
 print(name.title())
- 
---
+++ calculator.py
@@ -0,0 +1,13 @@
+#calculator in python
+# outhor : faustino Henriques 2025 
+
+from calendar import firstweekday
+
+
+first_value   = int(input("insert the value "))
+second_value = int(input(" insert your second value "))
+
+#calculatino the fisrt and second value
+
+total = first_value + second_value 
+print(f"the total is : {total}")
