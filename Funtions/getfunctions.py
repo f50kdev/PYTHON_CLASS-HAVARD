09a1be70186a4f07b96129eb7cbc7f42cf39d469
f50kdev <f50kdev@example.com>
2025-11-25 17:35:31 +0530

    update

---
+++ Funtions/getfunctions.py
@@ -0,0 +1,6 @@
+def get_guess():
+  guess = input("Enter the value")
+  return guess
+
+def  main():
+  guess = get_guess()
--- Funtions/hellos.py
+++ Funtions/hellos.py
@@ -2,5 +2,25 @@ def main():
   print("helloe world")
   print("hello CS50")
 
-main()
+
+
+def guess():
+  guesss = 10
+  print(guesss)
+#guess()
+
+def getguess():
+  guess = 10
+  return 10
+
+print(getguess())
+
+def variables():
+  advinhar  = input("insira o seu valor")
+  print(advinhar)
+  return advinhar
+
+
+variables()
+
 
